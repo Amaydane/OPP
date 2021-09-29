4c177f535d1da868ef938689a63478d39837fbd7
Amaydane <53432833+Amaydane@users.noreply.github.com>
2021-09-30 01:25:53 +0300

    Add files via upload

---
+++ Problem2/Problem2/Problem2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename Num>
+void swap(Num** a, Num n) {
+    int tmp;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            tmp = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = tmp;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int** a = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        a[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) cin >> a[i][j];
+    }
+    cout << "\n";
+    swap(a, n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) cout << a[i][j] << " ";
+        cout << "\n";
+    }
+    for (int i = 0; i < n; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+}
+
